0e6b31503e9394bcdb8719fd6c1ef136801e3f86
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-09 23:14:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,5 +6,14 @@ variable "private_key" {
 }
 variable "region" {}
 variable "compartment_ocid" {}
-variable "ssh_public_key" {}
 
+variable "ssh_public_key" {
+  description = "SSH public key to access the VM"
+  type        = string
+}
+
+variable "private_key" {
+  description = "Optional private key (if you are storing it in TF cloud)"
+  type        = string
+  default     = ""
+}
