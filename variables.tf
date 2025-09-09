0945141ea2562e0e54196449a416f098b6eaaa34
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-09 18:55:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "compartment_ocid" {
-  description = "ocid1.tenancy.oc1..aaaaaaaaumbbmktecpk33x3t6nw7uqfrlkshni4vb4qkwrdxdjfo5467v5ga"
+  description = "Root compartment (palomocloud)"
+  default     = "ocid1.tenancy.oc1..aaaaaaaaumbbmktecpk33x3t6nw7uqfrlkshni4vb4qkwrdxdjfo5467v5ga"
 }
