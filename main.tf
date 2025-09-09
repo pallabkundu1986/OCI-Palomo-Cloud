5a54828b5c961dcd5fc6934503fb70b878eee8e2
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-10 00:49:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Lookup Oracle Linux Image
 data "oci_core_images" "oracle_linux" {
-  compartment_id           = var.compartment_ocid
+  compartment_id           = var.tenancy_ocid
   operating_system         = "Oracle Linux"
   operating_system_version = "9"
   shape                    = "VM.Standard.E2.1.Micro"
@@ -13,32 +13,34 @@ data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
 
+# VCN
 resource "oci_core_vcn" "fin_vcn" {
   cidr_block     = "10.0.0.0/16"
   display_name   = "fin-vcn"
   compartment_id = var.compartment_ocid
 }
 
+# Subnet
 resource "oci_core_subnet" "public_subnet" {
-  vcn_id              = oci_core_vcn.fin_vcn.id
-  cidr_block          = "10.0.1.0/24"
-  display_name        = "public-subnet"
-  compartment_id      = var.compartment_ocid
+  vcn_id                    = oci_core_vcn.fin_vcn.id
+  cidr_block                = "10.0.1.0/24"
+  display_name              = "public-subnet"
+  compartment_id            = var.compartment_ocid
   prohibit_public_ip_on_vnic = false
 }
 
-# VM Instance (Only)
+# VM Instance
 resource "oci_core_instance" "linux_vm" {
   availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
   compartment_id      = var.compartment_ocid
   shape               = "VM.Standard.E2.1.Micro"
   display_name        = "Fin-vm"
 
-create_vnic_details {
-  subnet_id        = oci_core_subnet.public_subnet.id
-  assign_public_ip = true
-  hostname_label   = "finvm"
-}
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.public_subnet.id
+    assign_public_ip = true
+    hostname_label   = "finvm"
+  }
 
   source_details {
     source_type = "image"
@@ -49,5 +51,3 @@ create_vnic_details {
     ssh_authorized_keys = var.ssh_public_key
   }
 }
-
-
