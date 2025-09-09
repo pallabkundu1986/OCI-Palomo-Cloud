9cc19a773df6723a59bd69e51aaa5aefbf78a650
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-09 18:20:50 +0530

    Create new workspace

---
+++ main.tf
@@ -0,0 +1,10 @@
+data "oci_core_images" "oracle_linux" {
+  compartment_id = var.compartment_ocid
+  operating_system = "Oracle Linux"
+  operating_system_version = "9"
+  shape = "VM.Standard.E2.1.Micro"
+  
+    tags = {
+    Name = "Fin_vm"
+  }
+}
