1694d21956edf71866539f44788d268dbf6a5ffa
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-11 17:24:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -158,11 +158,6 @@ resource "oci_core_instance" "linux_vm1" {
   shape               = "VM.Standard.E3.Flex"
   display_name        = "Public-Server01"
 
-  # For A1.Flex shape
-  shape_config {
-    ocpus         = 1
-    memory_in_gbs = 2
-  }
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.public_subnet.id
@@ -175,24 +170,8 @@ resource "oci_core_instance" "linux_vm1" {
     source_id   = data.oci_core_images.arm_image.images[0].id
 	
   }
-    metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-	user_data = base64encode(<<-EOT
-      #!/bin/bash
-      dnf update -y
-      dnf install -y java-17-openjdk wget unzip
-      # Install Tomcat 10
-      cd /opt
-      wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.16/bin/apache-tomcat-10.1.16.zip
-      unzip apache-tomcat-10.1.16.zip
-      mv apache-tomcat-10.1.16 tomcat
-      chown -R opc:opc tomcat
-      # Start Tomcat
-      /opt/tomcat/bin/startup.sh
-    EOT
-    )
-  }
 }
+
  # Create Linux VM 2 (Private Access) 
   resource "oci_core_instance" "linux_vm2" {
   availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
