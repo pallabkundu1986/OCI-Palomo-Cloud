5679d08bce00d9295a6419bc54608399fedc0cdd
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-11 14:57:56 +0530

    New resource created

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ data "oci_core_images" "oracle_linux" {
   compartment_id           = var.tenancy_ocid
   operating_system         = "Oracle Linux"
   operating_system_version = "9"
-  shape                    = "VM.Standard.E2.1"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
@@ -14,17 +13,17 @@ data "oci_identity_availability_domains" "ADs" {
 }
 
 # Create VCN
-resource "oci_core_vcn" "fin_vcn" {
+resource "oci_core_vcn" "palomo_vcn" {
   cidr_block     = "10.0.0.0/16"
-  display_name   = "fin-vcn"
+  display_name   = "palomo-vcn"
   compartment_id = var.compartment_ocid
-  dns_label      = "finvcn"
+  dns_label      = "palomovcn"
 }
 
-# Create Security List (Allow SSH, HTTP, HTTPS and all outbound)
+# Create Security List for Public Network (Allow SSH, HTTP, HTTPS and all outbound)
 resource "oci_core_security_list" "public_sl" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.fin_vcn.id
+  vcn_id         = oci_core_vcn.palomo_vcn.id
   display_name   = "public-security-list"
 
   ingress_security_rules {
@@ -68,9 +67,30 @@ resource "oci_core_security_list" "public_sl" {
   }
 }
 
+# Create Security List for Private Network (Allow All)
+resource "oci_core_security_list" "private_sl" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.palomo_vcn.id
+  display_name   = "private-security-list"
+
+  ingress_security_rules {
+    protocol = "6"  # TCP
+    source   = "10.0.1.0/24"
+    tcp_options {
+      min = 1
+      max = 65535
+    }
+  }
+  
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+}
+
 # Create Public Subnet and attach Security List
 resource "oci_core_subnet" "public_subnet" {
-  vcn_id                     = oci_core_vcn.fin_vcn.id
+  vcn_id                     = oci_core_vcn.palomo_vcn.id
   cidr_block                 = "10.0.1.0/24"
   display_name               = "public-subnet"
   compartment_id             = var.compartment_ocid
@@ -84,15 +104,30 @@ resource "oci_core_subnet" "public_subnet" {
   ]
 }
 
+# Create Private Subnet and attach Security List
+resource "oci_core_subnet" "private_subnet" {
+  vcn_id                     = oci_core_vcn.palomo_vcn.id
+  cidr_block                 = "10.0.2.0/24"
+  display_name               = "private_subnet"
+  compartment_id             = var.compartment_ocid
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "privatesubnet" 
+
+  # Attach security list
+  security_list_ids = [
+    oci_core_security_list.private_sl.id
+  ]
+}
+
 resource "oci_core_internet_gateway" "igw" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.fin_vcn.id
+  vcn_id         = oci_core_vcn.palomo_vcn.id
   display_name   = "fin-igw"
 }
 
 resource "oci_core_route_table" "public_rt" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.fin_vcn.id
+  vcn_id         = oci_core_vcn.palomo_vcn.id
   display_name   = "public-rt"
 
   route_rules {
@@ -102,17 +137,38 @@ resource "oci_core_route_table" "public_rt" {
   }
 }
 
-# Create Linux VM
-resource "oci_core_instance" "linux_vm" {
+# Create Linux VM 1 (Public Access)
+resource "oci_core_instance" "linux_vm1" {
   availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
   compartment_id      = var.compartment_ocid
-  shape               = "VM.Standard.E2.1.Micro"
+  shape               = "VM.Standard.E2.1"
   display_name        = "Public-Server01"
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.public_subnet.id
     assign_public_ip = true
     hostname_label   = "Public-vnic"
+  }
+  source_details {
+  source_type = "image"
+  source_id   = data.oci_core_images.oracle_linux.images[0].id
+  }
+    metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+}
+  
+ # Create Linux VM 2 (Private Access) 
+  resource "oci_core_instance" "linux_vm2" {
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
+  compartment_id      = var.compartment_ocid
+  shape               = "VM.Standard.E2.1.Micro"
+  display_name        = "Private-VM01"
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.private_subnet.id
+    assign_public_ip = false
+    hostname_label   = "Private-vnic"
   }
 
   source_details {
