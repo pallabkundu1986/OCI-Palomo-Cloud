876669c5abd05c3cb15d6ec218e8a62dc8a0ecc7
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-11 15:36:30 +0530

    lin7

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "oci_core_images" "oracle_linux" {
   compartment_id           = var.tenancy_ocid
   operating_system         = "Oracle Linux"
-  operating_system_version = "8"
+  operating_system_version = "7"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
--- providers.tf
+++ providers.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
+	  version = ">=7.17.0"
     }
   }
 }
