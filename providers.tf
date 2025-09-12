0085d7bec6983bc6477645a3167190173ba1d716
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-09-12 13:13:08 +0530

    Fix error in provider

--- providers.tf
+++ providers.tf
@@ -6,15 +6,6 @@ terraform {
     }
   }
 }
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = ">=7.17.0"
-    }
-  }
-}
-
 
 provider "oci" {
   tenancy_ocid  = var.tenancy_ocid
